6048ccc32694371b8b7469649231588488b9df98
hoseacodes <dominique11h@yahoo.com>
2021-01-23 19:43:00 -0600

    added react-icons

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 import { MdKeyboardArrowRight, MdKeyboardArrowDown } from 'react-icons/md'
+import styled from 'styled-components';
+
+const Title = styled.div`
+    display: flex;
+    align-items: center;
+`;
 
 const SideBar = () => {
     const [isStarted, setStarted] = useState("false");
@@ -19,11 +25,10 @@ const SideBar = () => {
                         <h2>About UI Heat</h2>
                     </ul>
                     <ul>
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                            <h2 onClick={() => setStarted(!isStarted)}>Getting Started
-                        </h2>
+                        <Title >
+                            <h2 onClick={() => setStarted(!isStarted)}>Getting Started </h2>
                             <MdKeyboardArrowRight style={{ paddingLeft: "20px", fontSize: '2rem' }} />
-                        </div>
+                        </Title>
                         <ul className={isStarted ? "gettingStarted" : null}>
                             <li>
                                 <a href="#">
@@ -43,7 +48,11 @@ const SideBar = () => {
                         </ul>
                     </ul>
                     <ul>
-                        <h2 onClick={() => setComponents(!isComponents)}> Components</h2>
+                        <Title >
+                            <h2 onClick={() => setComponents(!isComponents)}> Components</h2>
+                            <MdKeyboardArrowRight style={{ paddingLeft: "20px", fontSize: '2rem' }} />
+                        </Title >
+
                         <ul className={isComponents ? "components" : null}>
 
                             <li>
@@ -83,38 +92,10 @@ const SideBar = () => {
                         </ul>
                     </ul>
                     <ul>
-                        <h2 onClick={() => setContent(!isContent)}> Content</h2>
-                        <ul className={isContent ? "content" : null}>
-
-                            <li>
-                                <a href="#">
-                                    <p>Typography</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <p>Images</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </ul>
-                    <ul>
-                        <h2 onClick={() => setHelper(!isHelper)}> Helpers</h2>
-                        <ul className={isHelper ? "helper" : null}>
-                            <li>
-                                <a href="#">
-                                    <p>Position</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <p>Text truncation</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </ul>
-                    <ul>
-                        <h2 onClick={() => setCustom(!isCustom)}> Customization</h2>
+                        <Title >
+                            <h2 onClick={() => setCustom(!isCustom)}> Customization</h2>
+                            <MdKeyboardArrowRight style={{ paddingLeft: "20px", fontSize: '2rem' }} />
+                        </Title >
                         <ul className={isCustom ? "custom" : null}>
 
                             <li>
@@ -130,9 +111,49 @@ const SideBar = () => {
                         </ul>
                     </ul>
                     <ul>
-                        <h2 onClick={() => setStyle(!isStyle)}> Styles</h2>
-                        <ul className={isStyle ? "style" : null}>
 
+                        <Title >
+                            <h2 onClick={() => setContent(!isContent)}> Content</h2>
+                            <MdKeyboardArrowRight style={{ paddingLeft: "20px", fontSize: '2rem' }} />
+                        </Title >
+                        <ul className={isContent ? "content" : null}>
+
+                            <li>
+                                <a href="#">
+                                    <p>Typography</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <p>Images</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </ul>
+                    <ul>
+                        <Title >
+                            <h2 onClick={() => setHelper(!isHelper)}> Helpers</h2>
+                            <MdKeyboardArrowRight style={{ paddingLeft: "20px", fontSize: '2rem' }} />
+                        </Title >
+                        <ul className={isHelper ? "helper" : null}>
+                            <li>
+                                <a href="#">
+                                    <p>Position</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <p>Text truncation</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </ul>
+                    <ul>
+                        <Title >
+                            <h2 onClick={() => setStyle(!isStyle)}> Styles</h2>
+                            <MdKeyboardArrowRight style={{ paddingLeft: "20px", fontSize: '2rem' }} />
+                        </Title >
+                        <ul className={isStyle ? "style" : null}>
                             <li>
                                 <a href="#">
                                     <p>Basic</p>
@@ -146,7 +167,10 @@ const SideBar = () => {
                         </ul>
                     </ul>
                     <ul>
-                        <h2 onClick={() => setGuide(!isGuide)}> Guides</h2>
+                        <Title >
+                            <h2 onClick={() => setGuide(!isGuide)}> Guides</h2>
+                            <MdKeyboardArrowRight style={{ paddingLeft: "20px", fontSize: '2rem' }} />
+                        </Title >
                         <ul className={isGuide ? "guide" : null}>
 
                             <li>
